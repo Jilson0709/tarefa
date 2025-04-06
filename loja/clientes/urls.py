3adbd7cc7a20da125d64b1bc7e5b73837718ba35
Jilson0709 <play927528@gmail.com>
2025-04-05 19:38:00 -0700

    Add files via upload

---
+++ loja/clientes/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.listar_clientes, name='listar_clientes'),
+    path('cadastrar/', views.cadastrar_cliente, name='cadastrar_cliente'),
+    path('editar/<int:id>/', views.editar_cliente, name='editar_cliente'),
+    path('excluir/<int:id>/', views.excluir_cliente, name='excluir_cliente'),
+]
+
